703d16e1ace4244eb4a63601e916db4a7ffacfe9
Ashon Woodbury <a.woodbury01@gmail.com>
2020-03-21 11:21:08 -0400

    created initial conditionals

---
+++ script.js
@@ -0,0 +1,15 @@
+var likesToTravel = true;
+
+if(likesToTravel === true){
+    console.log("Bon voyage!");
+} else {
+    console.log("Enjoy your couch");
+}
+
+var favoriteFood = "bbq";
+
+if(favoriteFood === "fried pickles" || favoriteFood === "pepperoni rolls") {
+    console.log("Congratulations, You have excellent taste!")
+} else{
+    console.log("Clearly you have not tried fried pickles or pepperoni rolls")
+}
