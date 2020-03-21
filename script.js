3366dca09fb6a31ffe0c89db12057e87c08f23f8
Ashon Woodbury <a.woodbury01@gmail.com>
2020-03-21 14:53:40 -0400

    Loops practice

--- script.js
+++ script.js
@@ -1,79 +1,108 @@
-var likesToTravel = true;
+// var likesToTravel = true;
 
-if(likesToTravel === true){
-    console.log("Bon voyage!");
-} else {
-    console.log("Enjoy your couch");
-}
+// if(likesToTravel === true){
+//     console.log("Bon voyage!");
+// } else {
+//     console.log("Enjoy your couch");
+// }
 
-var favoriteFood = "bbq";
+// var favoriteFood = "bbq";
 
-if(favoriteFood === "fried pickles" || favoriteFood === "pepperoni rolls") {
-    console.log("Congratulations, You have excellent taste!")
-} else{
-    console.log("Clearly you have not tried fried pickles or pepperoni rolls")
-}
+// if(favoriteFood === "fried pickles" || favoriteFood === "pepperoni rolls") {
+//     console.log("Congratulations, You have excellent taste!")
+// } else{
+//     console.log("Clearly you have not tried fried pickles or pepperoni rolls")
+// }
 
-//Check Your Math
-if(1+1 > 5){
-    console.log("True fact!");
-} else{
-    console.log("LIES!")
-}
+// //Check Your Math
+// if(1+1 > 5){
+//     console.log("True fact!");
+// } else{
+//     console.log("LIES!")
+// }
 
-//Dog people vs Cat people
-var likesDogs = true;
+// //Dog people vs Cat people
+// var likesDogs = true;
 
-if(likesDogs === true){
-    console.log("You're a dog person");
-} else{
-    console.log("You're a cat person");
-}
+// if(likesDogs === true){
+//     console.log("You're a dog person");
+// } else{
+//     console.log("You're a cat person");
+// }
 
-//Generation gaps
-var birthYear = 1988;
+// //Generation gaps
+// var birthYear = 1988;
 
-if(birthYear <= 1988){
-    console.log("You're a Millennium");
-}
+// if(birthYear <= 1988){
+//     console.log("You're a Millennium");
+// }
 
-//Greetings
-var personObject = {
-    firstName:"Ashon",
-    preferredLanguage:"English",
-}
+// //Greetings
+// var personObject = {
+//     firstName:"Ashon",
+//     preferredLanguage:"English",
+// }
 
-if(personObject.preferredLanguage === "English"){
-    console.log("Hello,", personObject.firstName);
-}
+// if(personObject.preferredLanguage === "English"){
+//     console.log("Hello,", personObject.firstName);
+// }
 
-//Grandma's kitchen
-var time = "7:30 AM";
-var isHungry = true;
+// //Grandma's kitchen
+// var time = "7:30 AM";
+// var isHungry = true;
 
-if(time === "7:30 AM" && isHungry === true){
-    console.log("Time for breakfast");
-} else if(time === "12:00 PM" && isHungry === true){
-    console.log("Time for lunch");
-} else if(time === "7:00 PM" || time === "8:00 PM" && isHungry === true){
-    console.log("Time for dinner");
-} else if(isHungry === true){
-    console.log("Time for a snack");
-} else{
-    console.log("Have a cookie anyway!");
-}
+// if(time === "7:30 AM" && isHungry === true){
+//     console.log("Time for breakfast");
+// } else if(time === "12:00 PM" && isHungry === true){
+//     console.log("Time for lunch");
+// } else if(time === "7:00 PM" || time === "8:00 PM" && isHungry === true){
+//     console.log("Time for dinner");
+// } else if(isHungry === true){
+//     console.log("Time for a snack");
+// } else{
+//     console.log("Have a cookie anyway!");
+// }
 
-//Student grades
-var grade = 70;
+// //Student grades
+// var grade = 70;
 
-if(grade >= 0 && grade <= 69){
-    console.log("You got an F");
-} else if(grade >= 70 && grade <= 76){
-    console.log("You got a D");
-} else if(grade >= 77 && grade <= 84){
-    console.log("You got a C");
-} else if(grade >= 85 && grade <= 92){
-    console.log("You got a B");
-} else if(grade >= 93 && grade <= 100){
-    console.log("You got an A");
+// if(grade >= 0 && grade <= 69){
+//     console.log("You got an F");
+// } else if(grade >= 70 && grade <= 76){
+//     console.log("You got a D");
+// } else if(grade >= 77 && grade <= 84){
+//     console.log("You got a C");
+// } else if(grade >= 85 && grade <= 92){
+//     console.log("You got a B");
+// } else if(grade >= 93 && grade <= 100){
+//     console.log("You got an A");
+// }
+
+// //While loop
+// var bird = 0;
+
+// while(bird < 100){
+//     console.log("Tweet Tweet")
+//     bird++;
+// }
+
+// //For loop
+// for(var i = 0; i < 100; i++){
+//     console.log("Psychotic bird", i);
+// }
+
+//Loops practice
+
+//Student names
+// var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"];
+    
+// for(var i = 0; i < cohortOneStudents.length; i++) {
+//     console.log(cohortOneStudents[i]);
+// }
+
+//Interests
+var interests = ["video games", "coding", "sitting inside", "not getting covid-19", "all of the above"];
+
+for(var i = 0; i < interests.length; i++){
+    console.log("One of my interests is", interests[i]);
 }
