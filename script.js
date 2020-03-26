b7186d37e0f27fd0a40f472315479c83b0b1937c
Ashon Woodbury <a.woodbury01@gmail.com>
2020-03-26 18:35:16 -0400

    Student grades take two

--- script.js
+++ script.js
@@ -100,9 +100,64 @@
 //     console.log(cohortOneStudents[i]);
 // }
 
-//Interests
-var interests = ["video games", "coding", "sitting inside", "not getting covid-19", "all of the above"];
+// Interests
+// var interests = ["video games", "coding", "sitting inside", "not getting covid-19", "all of the above"];
 
-for(var i = 0; i < interests.length; i++){
-    console.log("One of my interests is", interests[i]);
-}
+// for(var i = 0; i < interests.length; i++){
+//     console.log("One of my interests is", interests[i]);
+// }
+
+//Favorite interest
+// var interests = ["video games", "coding", "sitting inside", "not getting covid-19", "all of the above"];
+
+// for(var i = 0; i < interests.length; i++){
+//  if(interests[i] === "video games"){
+//     console.log("My favorite interest is", interests[i]);
+// } else{
+//     console.log("One of my interests is", interests[i]);
+// }
+// }
+
+//Harry Potter titles
+// var harryPotterTitles = ["and the Sorcerer's Stone", "and the Chamber of Secrets", "and the Prisoner of Azkaban", "and the Goblet of Fire", "and the Order of the Phoenix", "and the Half-Blood Prince", "and the Deathly Hallows"];
+
+// for(var i = 0; i < harryPotterTitles.length; i++){
+//     console.log("Harry Potter", harryPotterTitles[i]);
+// }
+
+//Student grades
+// var grades = [92, 91, 75, 66, 52, 90, 83, 85, 64, 90, 72, 88, 77, 98, 100, 73, 92]
+
+// for(var i = 0; i < grades.length; i++){
+//     if(grades[i] >= 0 && grades[i] <= 69){
+//         console.log("You got an F");
+//     } else if(grades[i] >= 70 && grades[i] <= 76){
+//         console.log("You got a D");
+//     } else if(grades[i] >= 77 && grades[i] <= 84){
+//         console.log("You got a C");
+//     } else if(grades[i] >= 85 && grades[i] <= 92){
+//         console.log("You got a B");
+//     } else if(grades[i] >= 93 && grades[i] <= 100){
+//         console.log("You got an A");
+// }
+// }
+
+//Two, Four, Six, Eight - Who do we appreciate?! CHEER
+
+// for(var i=2; i<4; i+2){
+//     console.log(i[0], i[1], i[3], i[4], "Who do we appreciate?!")
+// }
+
+//Student Grades Take Two
+
+// var grades = [92, 91, 75, 66, 52, 90, 83, 85, 64, 90, 72, 88, 77, 98, 100, 73, 92]
+
+// for(var i=0; i<grades.length; i++){
+//     if(grades[i] = 100){
+//         console.log(grades[i])
+//     } else if(grade[i] = 52){
+//         console.log(grades[i])
+//     } else{
+//         console.log("This number is in the middle")
+//     }
+// }
